fe6bfaee706505cac5422685244692cfe06f3a3e
Silvan Fuhrer <silvan@auterion.com>
2022-12-20 13:29:34 +0100

    Rework flaps/spoilers logic
    - remove deprecated actuator_controls[INDEX_FLAPS/SPOILERS/AIRBRAKES]
    - use new topics flaps_setpoint.msg and spoilers_setpoint.msg to pass
      into control allocation
    - remove flaps/spoiler related fields from attitude_setpoint topic
    - CA: add possibility to map flaps/spoilers to any control surface
    - move flaps/spoiler pitch trimming to CA (previously called DTRIM_FLAPS/SPOILER)
    - move manual flaps/spoiler handling from rate to attitude controller
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_path_navigation/FixedwingPositionControl.cpp
+++ src/modules/fw_path_navigation/FixedwingPositionControl.cpp
@@ -69,6 +69,12 @@ FixedwingPositionControl::FixedwingPositionControl(bool vtol) :
 	_launch_detection_status_pub.advertise();
 	_landing_gear_pub.advertise();
 
+	_flaps_setpoint_pub.advertise();
+	_spoilers_setpoint_pub.advertise();
+
+	_flaps_setpoint_with_slewrate.setSlewRate(kFlapSlewRate);
+	_spoilers_setpoint_with_slewrate.setSlewRate(kSpoilersSlewRate);
+
 	_airspeed_slew_rate_controller.setSlewRate(ASPD_SP_SLEW_RATE);
 
 	/* fetch initial parameter values */
@@ -937,12 +943,6 @@ FixedwingPositionControl::control_auto_fixed_bank_alt_hold(const float control_i
 	}
 
 	_att_sp.pitch_body = get_tecs_pitch();
-}
-
-void
-FixedwingPositionControl::control_auto_descend(const float control_interval)
-{
-	// Hard-code descend rate to 0.5m/s. This is a compromise to give the system to recover,
 	// but not letting it drift too far away.
 	const float descend_rate = -0.5f;
 
@@ -1236,10 +1236,9 @@ FixedwingPositionControl::control_auto_loiter(const float control_interval, cons
 		// have to do this switch (which can cause significant altitude errors) close to the ground.
 		_tecs.set_altitude_error_time_constant(_param_fw_thrtc_sc.get() * _param_fw_t_h_error_tc.get());
 		airspeed_sp = (_param_fw_lnd_airspd.get() > FLT_EPSILON) ? _param_fw_lnd_airspd.get() : _param_fw_airspd_min.get();
-		_att_sp.apply_flaps = vehicle_attitude_setpoint_s::FLAPS_LAND;
-		_att_sp.apply_spoilers = vehicle_attitude_setpoint_s::SPOILERS_LAND;
+		_flaps_setpoint_with_slewrate.update(_param_fw_flaps_lnd_scl.get(), control_interval);
+		_spoilers_setpoint_with_slewrate.update(_param_fw_spoilers_lnd.get(), control_interval);
 		_new_landing_gear_position = landing_gear_s::GEAR_DOWN;
-
 	}
 
 	float target_airspeed = adapt_airspeed_setpoint(control_interval, airspeed_sp, _param_fw_airspd_min.get(),
@@ -1400,7 +1399,7 @@ FixedwingPositionControl::control_auto_takeoff(const hrt_abstime &now, const flo
 		_att_sp.thrust_body[0] = _runway_takeoff.getThrottle(_param_fw_thr_idle.get(), get_tecs_thrust());
 
 		// apply flaps for takeoff according to the corresponding scale factor set via FW_FLAPS_TO_SCL
-		_att_sp.apply_flaps = vehicle_attitude_setpoint_s::FLAPS_TAKEOFF;
+		_flaps_setpoint_with_slewrate.update(_param_fw_flaps_to_scl.get(), control_interval);
 
 		// retract ladning gear once passed the climbout state
 		if (_runway_takeoff.getState() > RunwayTakeoffState::CLIMBOUT) {
@@ -1713,9 +1712,9 @@ FixedwingPositionControl::control_auto_landing(const hrt_abstime &now, const flo
 
 	_att_sp.roll_body = constrainRollNearGround(_att_sp.roll_body, _current_altitude, terrain_alt);
 
-	// Apply flaps and spoilers for landing. Amount of deflection is handled in the FW attitdue controller
-	_att_sp.apply_flaps = vehicle_attitude_setpoint_s::FLAPS_LAND;
-	_att_sp.apply_spoilers = vehicle_attitude_setpoint_s::SPOILERS_LAND;
+	// Apply flaps and spoilers for landing
+	_flaps_setpoint_with_slewrate.update(_param_fw_flaps_lnd_scl.get(), control_interval);
+	_spoilers_setpoint_with_slewrate.update(_param_fw_spoilers_lnd.get(), control_interval);
 
 	// deploy gear as soon as we're in land mode, if not already done before
 	_new_landing_gear_position = landing_gear_s::GEAR_DOWN;
@@ -2086,8 +2085,10 @@ FixedwingPositionControl::Run()
 		_npfg.setPeriod(_param_npfg_period.get());
 
 		_att_sp.reset_integral = false;
-		_att_sp.apply_flaps = vehicle_attitude_setpoint_s::FLAPS_OFF;
-		_att_sp.apply_spoilers = vehicle_attitude_setpoint_s::SPOILERS_OFF;
+
+		// by default set flaps and spoilers to 0. Will be set below in takeoff and landing.
+		_flaps_setpoint_with_slewrate.update(0.f, control_interval);
+		_spoilers_setpoint_with_slewrate.update(0.f, control_interval);
 
 		// by default we don't want yaw to be contoller directly with rudder
 		_att_sp.fw_control_yaw_wheel = false;
@@ -2192,6 +2193,19 @@ FixedwingPositionControl::Run()
 			landing_gear.landing_gear = _new_landing_gear_position;
 			landing_gear.timestamp = hrt_absolute_time();
 			_landing_gear_pub.publish(landing_gear);
+		}
+
+		// In Manual modes flaps and spoilers are directly controlled in the Attitude controller and not published here
+		if (_control_mode.flag_control_auto_enabled) {
+			flaps_setpoint_s flaps_setpoint;
+			flaps_setpoint.normalized_setpoint = _flaps_setpoint_with_slewrate.getState();
+			flaps_setpoint.timestamp = hrt_absolute_time();
+			_flaps_setpoint_pub.publish(flaps_setpoint);
+
+			spoilers_setpoint_s spoilers_setpoint;
+			spoilers_setpoint.normalized_setpoint = _spoilers_setpoint_with_slewrate.getState();
+			spoilers_setpoint.timestamp = hrt_absolute_time();
+			_spoilers_setpoint_pub.publish(spoilers_setpoint);
 		}
 
 		perf_end(_loop_perf);
